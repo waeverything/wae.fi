ff9e66c175f7b21789cb5786d675af71f232fafd
fabianmakila <contact@fabianadrian.fi>
2023-04-04 23:24:28 +0300

    Update to the latest Astro version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 const colors = require('tailwindcss/colors')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
